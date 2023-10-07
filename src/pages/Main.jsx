71758aba9400784a6379fabad4b3638bf59d9776
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2023-10-07 16:55:08 -0600

    main page added grid

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,42 @@
+import { FaLocationDot } from "react-icons/fa6"
 import TabLayout from "../layouts/tabs"
+
+import water from "../data/water.json"
 
 export default function Main() {
   return (
     <TabLayout>
-      <div>
-        Pagina principal
+
+      <div className="mt-10">
+        <div className="flex items-center justify-center">
+          <FaLocationDot
+            className="w-5 h-5 mr-2 text-blue-700"
+          />
+          <h3 className="text-3xl font-semibold text-blue-700">
+            Abasolo
+          </h3>
+        </div>
       </div>
+
+      <div className="grid grid-cols-2 gap-4 mt-10">
+
+        {water.map((item) => (
+          <div
+            key={item.id}
+            className="flex flex-col justify-center p-3 bg-blue-100 rounded-xl"
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-32 mr-3 bg-contain rounded-xl"
+            />
+            <div className="flex items-center justify-center w-full mt-2">
+              {item.name}
+            </div>
+          </div>
+        ))}
+      </div>
+
     </TabLayout>
   )
 }
--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,9 +1,12 @@
+import SplashImage from '../assets/splash.png'
+
 export default function Splash() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-2xl font-bold text-blue-500">
-        Splash
-      </div>
+      <img src={SplashImage}
+        alt="Splash"
+        className="w-8/12"
+      />
     </div>
   )
 }
